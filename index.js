c2b1607e368e4d22db61713778d77b8baf7d47a8
Carley Tripp <carley.tripp@outlook.com>
2020-06-11 00:41:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+// Function declaration
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
+// Function expression called with mondayWork()
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
+
+function wrapAdjective(flair="*") {
+  return function(adjective="special") {
+    return `You are ${flair}${adjective}${flair}!`
+  }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!")
